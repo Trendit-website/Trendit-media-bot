75f172589f5820da518ee494be6425ac939d5ced
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2024-07-01 02:07:15 +0100

    correction:

--- bot/commands/profile.py
+++ bot/commands/profile.py
@@ -7,8 +7,8 @@ from ..extensions import api
 async def pending_social_profiles(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Send a message when the command /fetch_stats is issued."""
     response_data = api.webhook_get_pending_social_profiles()
-    response_data_msg = response_data["message"]
-    pending_social_profiles = response_data["pending_social_profiles"]
+    response_data_msg = response_data.get("message", "Fetching pending social profiles")
+    pending_social_profiles = response_data.get("pending_social_profiles", None)
     
     
     if pending_social_profiles:
